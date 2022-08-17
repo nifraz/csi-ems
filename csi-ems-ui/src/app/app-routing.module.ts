6e5353a510eee3dd786a4dff59260f1d2487875b
Nifraz Navahz <nifraz@live.com>
2022-08-17 14:39:36 +0530

    add routing

--- csi-ems-ui/src/app/app-routing.module.ts
+++ csi-ems-ui/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { EmployeeDetailComponent } from './employee/employee-detail/employee-detail.component';
+import { EmployeeEditComponent } from './employee/employee-edit/employee-edit.component';
+import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
+import { EmployeeComponent } from './employee/employee.component';
+
+const routes: Routes = [
+  {path: 'employees', component: EmployeeComponent, children: [
+      {path: '', component: EmployeeListComponent},
+      {path: 'new', component: EmployeeEditComponent},
+      {path: ':id', component: EmployeeDetailComponent},
+      {path: ':id/edit', component: EmployeeEditComponent},
+  ]},
+  {path: '', redirectTo: '/employees', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
