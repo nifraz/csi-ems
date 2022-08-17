c4f2e710b6469bf59ee68bd5ed678585a073d79c
Nifraz Navahz <nifraz@live.com>
2022-08-17 22:58:29 +0530

    implement service

--- csi-ems-ui/src/app/employee/employee.component.ts
+++ csi-ems-ui/src/app/employee/employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'app-employee',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit(): void {
+    this.employeeService.getEmployees().subscribe(res => {
+      console.log(res);
+    })
   }
 
 }
--- csi-ems-ui/src/app/employee/employee.service.ts
+++ csi-ems-ui/src/app/employee/employee.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Employee } from './employee.model';
 
+const apiUrl = 'http://localhost:5001/api';
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getEmployees() {
+    return this.httpClient.get<any>(apiUrl);
+  }
+
+  getEmployee(id: string){
+    return this.httpClient.get<Employee>(apiUrl, 
+      { params: {'id': id }});
+  }
+
+  createEmployee(newEmployee: Employee) {
+    return this.httpClient.post<Employee>(apiUrl, newEmployee);
+  }
+
+  updateEmployee(id: string, updatedEmployee: Employee){
+    return this.httpClient.put<Employee>(apiUrl, updatedEmployee,
+      { params: {'id': id }});
+  }
+
+  deleteEmployee(id: string){
+    return this.httpClient.delete(apiUrl, 
+      { params: {'id': id }});
+  }
+  
 }
